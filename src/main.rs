ac3339dc3f777e8b204f3f367ab64097f603eb04
TylerIsFlying <54076402+TylerIsFlying@users.noreply.github.com>
2024-02-26 01:29:35 -0800

    Add let & printout age
    Testing out how lets and println work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+fn main(){
+    let num: i32 = 40;
+    let num2: i32 = 41;
+    let pair: (char, i32) = ('b',17);
+    let p = pair.0;
+    println!("Age = {1}, Age = {0} \n Hello {p}", num, num2)
 }
